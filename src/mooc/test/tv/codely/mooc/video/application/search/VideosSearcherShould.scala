3554bfbf078b84adb8d63d5e17c1eb099fc3380d
acortijo <alex.cortijo117@gmail.com>
2020-09-09 22:55:23 +0200

    Fix Unit Test of finding existing video (delete save)

--- src/mooc/test/tv/codely/mooc/video/application/search/VideosSearcherShould.scala
+++ src/mooc/test/tv/codely/mooc/video/application/search/VideosSearcherShould.scala
@@ -1,14 +1,11 @@
 package tv.codely.mooc.video.application.search
 
-import tv.codely.mooc.user.domain.UserIdMother
 import tv.codely.mooc.video.domain._
 import tv.codely.mooc.video.infrastructure.repository.VideoRepositoryMock
 import tv.codely.shared.infrastructure.unit.UnitTestCase
 
 final class VideosSearcherShould extends UnitTestCase with VideoRepositoryMock {
   private val searcher = new VideosSearcher(repository)
-//  private val publisher = mock[MessagePublisher]
-//  private val creator = new VideoCreator(repository, publisher)
 
   "search all existing videos" in {
     val existingVideo        = VideoMother.random
@@ -21,17 +18,10 @@ final class VideosSearcherShould extends UnitTestCase with VideoRepositoryMock {
   }
 
   "search an existing video" in {
-    val videoId = VideoIdMother.random
-    val videoTitle = VideoTitleMother.random
-    val videoDuration = VideoDurationMother.random
-    val videoCategory = VideoCategoryMother.random
-    val userId = UserIdMother.random
-    val existingVideo = Video(videoId, videoTitle, videoDuration, videoCategory, userId)
+    val existingVideo = VideoMother.random
 
-    repositoryShouldSave(existingVideo)
     repositoryShouldFindVideo(existingVideo)
 
-    repository.save(existingVideo)
     searcher.find(existingVideo.id).futureValue.get shouldBe existingVideo
 
   }
